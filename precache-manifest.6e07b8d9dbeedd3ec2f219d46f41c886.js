a6ebe94fb0bc72461618f7688981284d44feab96
Pankaj Dharmik <pankajdharmik4@gmail.com>
2022-06-14 16:33:32 +0530

    Updates

---
+++ precache-manifest.6e07b8d9dbeedd3ec2f219d46f41c886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7843b4db610af9f26030",
+    "url": "/text-utils/static/css/main.895f2266.chunk.css"
+  },
+  {
+    "revision": "7843b4db610af9f26030",
+    "url": "/text-utils/static/js/main.7843b4db.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/text-utils/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "18a9211fd047086487e8",
+    "url": "/text-utils/static/js/2.18a9211f.chunk.js"
+  },
+  {
+    "revision": "7d770d2e8699e050de64",
+    "url": "/text-utils/static/js/runtime~main.7d770d2e.js"
+  },
+  {
+    "revision": "65535730356f3c45ed617f4d5a41126c",
+    "url": "/text-utils/index.html"
+  }
+];
